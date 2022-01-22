569486621c25c6b48818487c9aa2ac40b550b675
Bonsi <amonselltapia@gmail.com>
2022-01-22 16:08:33 +0100

    crud users

--- controller/userController.js
+++ controller/userController.js
@@ -1,4 +1,5 @@
 const { User } = require("../models/index.js");
+const user = require("../models/user.js");
 
 const UserController = {
   create(req, res) {
@@ -8,6 +9,27 @@ const UserController = {
         res.status(201).send({ message: "Usuario creado con éxito", user })
       )
       .catch(console.error);
+  },
+  showUsers(req, res) {
+    User.findAll().then((users) => res.send(users));
+  },
+  userById(req, res) {
+    User.findByPk(req.params.id).then((user) => res.send(user));
+  },
+  async updateUser(req, res) {
+    try {
+      let user = await User.update(
+        { ...req.body },
+        {
+          where: {
+            id: req.params.id,
+          },
+        }
+      );
+      res.send("Usuario Actualizado");
+    } catch (error) {
+      console.log(error);
+    }
   },
 };
 
--- routes/user.js
+++ routes/user.js
@@ -3,5 +3,9 @@ const router = express.Router();
 const UserController = require("../controller/userController");
 
 router.post("/create", UserController.create);
+router.get("/", UserController.showUsers);
+router.get("/:id", UserController.userById);
+router.put("/:id", UserController.updateUser);
+//router.delete()
 
 module.exports = router;
